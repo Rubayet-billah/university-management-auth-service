aaa7e764e926b51c91757a87d6d7c31751237cf3
Rubayet-billah <rubayetbillah909@gmail.com>
2023-06-02 01:50:31 +0600

    Done: ESLint, Prettier, Husky, LintStage setup

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,18 @@
-import express, { Application } from "express";
-import cors from "cors";
+import express, { Application } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
 // cors
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.use(cors());
+app.use(cors())
 
 // parser middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
